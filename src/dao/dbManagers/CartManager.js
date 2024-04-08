3fd64348c93bd31fc74b6555ef6ce2361e60aa71
VeroPizzato <verizzato@gmail.com>
2024-04-08 13:28:12 -0300

    subo cambio

--- src/dao/dbManagers/CartManager.js
+++ src/dao/dbManagers/CartManager.js
@@ -2,6 +2,8 @@ const CartModel = require('../models/cart')
 
 class CartManager {
 
+    static #ultimoIdCart = 1
+
     constructor() { }
 
     inicialize = async () => {
@@ -10,6 +12,20 @@ class CartManager {
         if (CartModel.db.readyState !== 1) {
             throw new Error('must connect to mongodb!')
         }
+        else {
+            const carts = await this.getCarts()
+            CartManager.#ultimoIdCart = this.#getNuevoIdInicio(carts)
+        }
+    }
+
+    #getNuevoIdInicio = (carts) => {
+        let mayorID = 1
+        carts.forEach(item => {
+            if (mayorID <= item.id)
+                mayorID = item.id
+        });
+        mayorID = mayorID + 1
+        return mayorID
     }
 
     getCarts = async () => {
@@ -22,21 +38,32 @@ class CartManager {
         }
     }
 
+    #getNuevoId() {
+        const id = CartManager.#ultimoIdCart
+        CartManager.#ultimoIdCart++
+        return id
+    }
+
     getCartByCId = async (cid) => {
-        const cart = await CartModel.findById(cid)
-        //const cart = await CartModel.findOne(cid)
-        console.log(cart)
+        const cart = await CartModel.findOne({ id: cid })
+        if (cart)
+            return cart
+        else {
+            console.error(`Carrito con ID: ${cid} Not Found`)
+            return
+        }
     }
 
     addCart = async (arrayCart) => {
         let nuevoCarrito = await CartModel.create({
+            id: this.#getNuevoId(),
             arrayCart
         })
         console.log(nuevoCarrito)
     }
 
     deleteProductToCart = async (pid) => {
-        let result = await CartModel.deleteOne({ _id: pid })
+        let result = await CartModel.deleteOne({ id: pid })
     }
 }
 
--- src/dao/models/cart.js
+++ src/dao/models/cart.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose')
 const cartCollection = 'carts';
 
 const cartSchema = new mongoose.Schema({
+    id: {
+        type: Number,
+        requied: true,
+        unique: true
+    },
     arrayCart: [{ productId: String, quantity: Number }]    
 });
 
